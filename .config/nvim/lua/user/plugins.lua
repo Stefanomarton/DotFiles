2308f5ed8a5548baa22312ee0659116572d6583c
Stefanomarton <sstefanomarton@gmail.com>
2022-11-01 15:07:30 +0100

    Reworks

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,204 +1,200 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 --
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 return packer.startup(function(use)
+	use({ "wbthomason/packer.nvim" })
+	use({ "nvim-lua/plenary.nvim" })
+	use({ "windwp/nvim-autopairs" })
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "kyazdani42/nvim-tree.lua", commit = "ce463a5" })
+	use({ "moll/vim-bbye" })
+	use({ "nvim-lualine/lualine.nvim" })
+	use({ "lewis6991/impatient.nvim" })
+	use({ "lukas-reineke/indent-blankline.nvim" })
+	use({ "L3MON4D3/LuaSnip" })
+	use({ "nvim-treesitter/nvim-treesitter" })
+	use({ "nvim-treesitter/nvim-treesitter-context" })
 
-  use { "wbthomason/packer.nvim" }
-  use { "nvim-lua/plenary.nvim" }
-  use { "windwp/nvim-autopairs" }
-  use { "kyazdani42/nvim-web-devicons" }
-  use { "kyazdani42/nvim-tree.lua", commit = "ce463a5" }
-  use { "moll/vim-bbye" }
-  use { "nvim-lualine/lualine.nvim" }
-  use { "lewis6991/impatient.nvim" }
-  use { "lukas-reineke/indent-blankline.nvim" }
-  use { "L3MON4D3/LuaSnip" }
-  use { "nvim-treesitter/nvim-treesitter" }
-  use {'nvim-treesitter/nvim-treesitter-context'}
+	--Colorscheme
+	use({ "dracula/vim" })
+	use({ "folke/tokyonight.nvim" })
+	use({ "akinsho/bufferline.nvim" })
 
-  --Colorscheme
-  use { "dracula/vim" }
-  use { 'folke/tokyonight.nvim' }
-  use { "akinsho/bufferline.nvim" }
+	use({ "williamboman/nvim-lsp-installer" })
+	use({ "j-hui/fidget.nvim" })
 
+	-- use { "jose-elias-alvarez/null-ls.nvim"}
+	use({ "numToStr/Comment.nvim" })
 
-  use { "neovim/nvim-lspconfig" }
-  use { "williamboman/nvim-lsp-installer" }
-  use { "j-hui/fidget.nvim" }
+	use({ "https://github.com/junegunn/fzf.vim.git" })
 
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "saadparwaiz1/cmp_luasnip" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-nvim-lua" })
+	use({ "hrsh7th/cmp-cmdlIne" })
 
-  -- use { "jose-elias-alvarez/null-ls.nvim"}
-  use { "numToStr/Comment.nvim"}
+	use({ "unblevable/quick-scope" })
+	use({ "lervag/vimtex" })
 
-  use { "https://github.com/junegunn/fzf.vim.git" }
+	use({ "goolord/alpha-nvim" })
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+	use({ "nvim-telescope/telescope.nvim" })
+	use({ "glepnir/galaxyline.nvim" })
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
 
-  use { "hrsh7th/nvim-cmp" }
-  use { "hrsh7th/cmp-buffer" }
-  use { "hrsh7th/cmp-path" }
-  use { "saadparwaiz1/cmp_luasnip" }
-  use { "hrsh7th/cmp-nvim-lsp" }
-  use { "hrsh7th/cmp-nvim-lua" }
-  use { "hrsh7th/cmp-cmdlIne" }
+	-- use 'ggandor/lightspeed.nvim'
 
-  use { "unblevable/quick-scope" }
-  use { 'lervag/vimtex' }
+	use({
+		"mvllow/modes.nvim",
+		tag = "v0.2.0",
+		config = function()
+			require("modes").setup()
+		end,
+	})
 
-  use { 'goolord/alpha-nvim' }
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig"}
-  use { 'nvim-telescope/telescope.nvim' }
-  use { 'glepnir/galaxyline.nvim' }
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+	use({
+		"phaazon/hop.nvim",
+		branch = "v2", -- optional but strongly recommended
+		config = function()
+			-- you can configure Hop the way you like here; see :h hop-config
+			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+		end,
+	})
 
-  -- use 'ggandor/lightspeed.nvim'
+	use({
+		"xuhdev/vim-latex-live-preview",
+	})
 
-  use({
-    'mvllow/modes.nvim',
-    tag = 'v0.2.0',
-    config = function()
-      require('modes').setup()
-    end
-  })
+	use({ "folke/which-key.nvim" })
 
-  use {
-    'phaazon/hop.nvim',
-    branch = 'v2', -- optional but strongly recommended
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-    end
-  }
+	use({ "jose-elias-alvarez/null-ls.nvim" })
 
-  use {
-    'xuhdev/vim-latex-live-preview'
-  }
+	use({ "folke/trouble.nvim" })
 
+	use({ "NvChad/nvim-colorizer.lua" })
 
-  use { "folke/which-key.nvim" }
+	use({ "lvimuser/lsp-inlayhints.nvim" })
 
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
 
-  use { "jose-elias-alvarez/null-ls.nvim" }
+	use({
+		"abecodes/tabout.nvim",
+		config = function()
+			require("tabout").setup({
+				tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+				backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+				act_as_tab = true, -- shift content if tab out is not possible
+				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+				default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+				default_shift_tab = "<C-d>", -- reverse shift default action,
+				enable_backwards = true, -- well ...
+				completion = true, -- if the tabkey is used in a completion pum
+				tabouts = {
+					{ open = "'", close = "'" },
+					{ open = '"', close = '"' },
+					{ open = "`", close = "`" },
+					{ open = "(", close = ")" },
+					{ open = "[", close = "]" },
+					{ open = "{", close = "}" },
+				},
+				ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+				exclude = {}, -- tabout will ignore these filetypes
+			})
+		end,
+		wants = { "nvim-treesitter" }, -- or require if not used so far
+		after = { "nvim-cmp" }, -- if a completion plugin is using tabs load it before_init
+	})
 
-  use { "folke/trouble.nvim" }
+	use({
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").set_default_keymaps()
+		end,
+	})
 
-  use { 'NvChad/nvim-colorizer.lua' }
+	use({ "rcarriga/nvim-notify" })
 
-  use { "lvimuser/lsp-inlayhints.nvim" }
+	use({
+		"Pocco81/true-zen.nvim",
+	})
 
-  use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  })
+	use({ "mhartington/formatter.nvim" })
 
+	-- Packer
+	use({
+		"folke/noice.nvim",
+		event = "VimEnter",
+		config = function()
+			require("noice").setup()
+		end,
+		requires = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- "rcarriga/nvim-notify",
+		},
+	})
 
-  use { "abecodes/tabout.nvim",
-    config = function()
-      require('tabout').setup {
-        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-        act_as_tab = true, -- shift content if tab out is not possible
-        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-        default_shift_tab = '<C-d>', -- reverse shift default action,
-        enable_backwards = true, -- well ...
-        completion = true, -- if the tabkey is used in a completion pum
-        tabouts = {
-          { open = "'", close = "'" },
-          { open = '"', close = '"' },
-          { open = '`', close = '`' },
-          { open = '(', close = ')' },
-          { open = '[', close = ']' },
-          { open = '{', close = '}' }
-        },
-        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-        exclude = {} -- tabout will ignore these filetypes
-      }
-    end,
-    wants = { 'nvim-treesitter' }, -- or require if not used so far
-    after = { 'nvim-cmp' } -- if a completion plugin is using tabs load it before_init
-  }
-
-
-use { "ggandor/leap.nvim",
-  config = function ()
- require('leap').set_default_keymaps()
-    end }
-
-use { "rcarriga/nvim-notify"}
-
-use({
-	"Pocco81/true-zen.nvim"
-})
-
-use { 'mhartington/formatter.nvim' }
-
--- Packer
-use({
-  "folke/noice.nvim",
-  event = "VimEnter",
-  config = function()
-    require("noice").setup()
-  end,
-  requires = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    -- "rcarriga/nvim-notify",
-    }
-})
-
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
