1659d6c422139266713733aeda6a11a12bf7b723
Stefanomarton <sstefanomarton@gmail.com>
2022-11-16 21:12:37 +0100

    add: noice.nvim config

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -5,4 +5,5 @@ wk.register({
 	p = { ":!zathura %:r.pdf > /dev/null 2>&1 &<cr><cr>", "Preview", noremap = true, silent = true },
 	s = { ":Telescope heading <cr>", "Toc", noremap = true, silent = true },
 	l = { ":Telescope luasnip theme=get_cursor<cr>", "Snippets", noremap = true, silent = true },
+	r = { ":<F6><cr>", "Snippets", noremap = true, silent = true },
 }, { prefix = "<leader>" })
