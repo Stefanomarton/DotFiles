73c24f127d082e083b679ff003d034f9edfd6b0d
Stefanomarton <sstefanomarton@gmail.com>
2022-12-04 19:39:58 +0100

    .

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -34,7 +34,7 @@ vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would sh
 vim.opt.wrap = true -- display lines as one long line
 vim.opt.scrolloff = 8 -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "JetBrainsMono Nerd Font:h20" -- the font used in graphical neovim applications
+vim.opt.guifont = "JetBrainsMono Nerd Font:h17" -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = " "
 vim.opt.shortmess:append("c")
 vim.opt.whichwrap:append("<,>,[,],h,l")
