e0fbe7b6c8d56f4954bf3dc4229af0c114fab0a5
Stefanomarton <sstefanomarton@gmail.com>
2022-12-06 00:15:23 +0100

    ..

--- .config/emacsvecchio/elpa/centered-cursor-mode-20200507.1529/centered-cursor-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from centered-cursor-mode.el  -*- no-byte-compile: t -*-
-(define-package "centered-cursor-mode" "20200507.1529" "cursor stays vertically centered" 'nil :commit "e9ee518d314ef7fb105a1e0cdc33dbb4dfe9d9e4" :authors '(("André Riemann" . "andre.riemann@web.de")) :maintainer '("André Riemann" . "andre.riemann@web.de") :keywords '("convenience") :url "https://github.com/andre-r/centered-cursor-mode.el")
--- .config/emacsvecchio/elpa/evil-tex-20221021.1344/evil-tex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from evil-tex.el  -*- no-byte-compile: t -*-
-(define-package "evil-tex" "20221021.1344" "Useful features for editing LaTeX in evil-mode" '((emacs "26.1") (evil "1.0") (auctex "11.88")) :commit "97616a81fb88ad558051b57d899fdaba56d8d7fa" :keywords '("tex" "emulation" "vi" "evil" "wp") :url "https://github.com/iyefrat/evil-tex")
