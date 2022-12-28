7ae86c087f35afd5ba03a955506ca330bd6c4ba8
Stefanomarton <sstefanomarton@gmail.com>
2022-12-28 15:55:45 +0100

    changed colorscheme

--- .config/nvim/lua/user/deco/colorscheme.lua
+++ .config/nvim/lua/user/deco/colorscheme.lua
@@ -1,5 +1,5 @@
 function CScheme(color)
-	color = color or "tokyonight"
+	color = color or "nord"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(20, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(10, "NormalFloat", { bg = "none" })
