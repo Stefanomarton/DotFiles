119f346b8772cfba93964af172531c4a6d3861d8
Stefanomarton <sstefanomarton@gmail.com>
2022-12-30 21:44:05 +0100

    delete useless xplr config

--- .config/xplr/init.lua
+++
@@ -1,5 +0,0 @@
-local xplr = xplr
-version = "0.20.0"
-xplr.config.general.disable_debug_error_mode = true
-xplr.config.general.enforce_bounded_index_navigation = true
-xplr.config.general.enable_mouse = true
