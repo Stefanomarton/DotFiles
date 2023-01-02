1835eb93f5facf4deed22ca53aed4203f4fd8e73
Stefanomarton <sstefanomarton@gmail.com>
2023-01-02 12:23:59 +0100

    fix colorscheme

--- .config/nvim/lua/user/deco/colorscheme.lua
+++ .config/nvim/lua/user/deco/colorscheme.lua
@@ -1,8 +1,8 @@
 function CScheme(color)
-	color = color or "nord"
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(20, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(10, "NormalFloat", { bg = "none" })
+color = color or "nord"
+vim.cmd.colorscheme(color)
+vim.api.nvim_set_hl(20, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(10, "NormalFloat", { bg = "none" })
 end
 
 CScheme()
