85dd8031303611183649e8b07bf27849ed53a850
Stefanomarton <sstefanomarton@gmail.com>
2023-01-21 20:43:59 +0100

    feat: cattpuccin plugin

--- .config/nvim/lua/user/deco/colorscheme.lua
+++ .config/nvim/lua/user/deco/colorscheme.lua
@@ -1,8 +1,8 @@
-function CScheme(color)
-	color = color or "nord"
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(10, "Normal", { bg = "#2e3440" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2e3440" })
-end
-
-CScheme()
+-- function CScheme(color)
+-- 	color = color or "nord"
+-- 	vim.cmd.colorscheme(color)
+-- 	vim.api.nvim_set_hl(10, "Normal", { bg = "#2e3440" })
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2e3440" })
+-- end
+--
+-- CScheme()
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -121,17 +121,24 @@ return packer.startup(function(use)
 	--Colorscheme and deco stuff
 	use({
 		"nvim-lualine/lualine.nvim",
-		after = "nord.nvim",
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "nord",
+					theme = "catppuccin",
 				},
 			})
 		end,
 	})
 
-	use("shaunsingh/nord.nvim")
+	-- use({
+	-- 	"shaunsingh/nord.nvim",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("nord")
+	-- 		vim.g.nord_borders = true
+	-- 		vim.g.nord_contrast = true
+	-- 		require("nord").set()
+	-- 	end,
+	-- })
 
 	use({
 		"petertriho/nvim-scrollbar",
@@ -470,6 +477,57 @@ The Core plugins
 				n_lines = 50,
 				search_method = "cover_or_next",
 			})
+		end,
+	})
+	use({
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "frappe",
+					dark = "latte",
+				},
+				transparent_background = true,
+				show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+				term_colors = false,
+				dim_inactive = {
+					enabled = true,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				no_italic = false, -- Force no italic
+				no_bold = false, -- Force no bold
+				styles = {
+					comments = { "italic" },
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					notify = false,
+					mini = true,
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
+			})
+
+			-- setup must be called before loading
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	})
 
