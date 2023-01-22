b31130d682e472365602e6ac13bf5449f18aba5b
Stefanomarton <sstefanomarton@gmail.com>
2023-01-22 19:59:41 +0100

    fix: ignore elpa archives

--- .config/emacs/elpa/google-this-20170810.1215/google-this-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from google-this.el  -*- no-byte-compile: t -*-
-(define-package "google-this" "20170810.1215" "A set of functions and bindings to google under point." '((emacs "24.1")) :commit "8a2e3ca5da6a8c89bfe99a21486c6c7db125dc84" :authors '(("Artur Malabarba" . "bruce.connor.am@gmail.com")) :maintainer '("Artur Malabarba" . "bruce.connor.am@gmail.com") :keywords '("convenience" "hypermedia") :url "http://github.com/Malabarba/emacs-google-this")
