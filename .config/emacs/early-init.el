726c68d180ddcb609460f6ee531542c573919eee
Stefanomarton <sstefanomarton@gmail.com>
2023-01-27 10:23:09 +0100

    feat: add early-init with minimal UI e startup optimisation

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,13 @@
+;; Better startup performance
+(setq read-process-output-max (* 1024 1024))
+(setq gc-cons-threshold 100000000)
+
+;; Minimal UI
+(setq-default
+ package-native-compile t
+ default-frame-alist
+ '((tool-bar-lines . 0)
+   (menu-bar-lines . 0)
+   (undecorated . t)
+   (vertical-scroll-bars . nil)
+   (horizontal-scroll-bars . nil)))
