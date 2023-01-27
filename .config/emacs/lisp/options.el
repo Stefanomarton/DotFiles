769f897bf03a85101d4bac2ad34eb29a1580e4f4
Stefanomarton <sstefanomarton@gmail.com>
2023-01-27 10:21:56 +0100

    fix: remove bloat and redundand messages

--- .config/emacs/lisp/options.el
+++ .config/emacs/lisp/options.el
@@ -2,7 +2,7 @@
 
 ;; Provide user name and email adress
 (setq user-full-name "Stefano Marton"
-      user-mail-address "sstefanomarton@gmail.com")
+			user-mail-address "sstefanomarton@gmail.com")
 
 ;; UI settings
 (add-to-list 'default-frame-alist '(alpha 95 95)) ;; opacity settings
@@ -12,6 +12,16 @@
 (tooltip-mode -1) ;; no tooltip
 (menu-bar-mode -1) ;; no menu bar
 (set-fringe-mode 15) ;; Set the padding of the inside windows
+(setq inhibit-startup-screen t
+			initial-scratch-message nil
+			sentence-end-double-space nil
+			ring-bell-function 'ignore
+			frame-resize-pixelwise t)
+
+;; Remove useless whitespace before saving a file
+(setq-default nuke-trailing-whitespace-p t)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
 
 (use-package linum-relative
 	:straight t
@@ -19,24 +29,19 @@
 	(linum-relative-global-mode))
 
 (global-hl-line-mode) ;; Highlight the current line
-(setq inhibit-startup-screen t
-		  initial-scratch-message nil
-      sentence-end-double-space nil
-      ring-bell-function 'ignore
-      frame-resize-pixelwise t)
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 
-(use-package centered-cursor-mode				;Add centered cursor 
+(use-package centered-cursor-mode				;Add centered cursor
 	:straight t
 	:init
-  (global-centered-cursor-mode))
+	(global-centered-cursor-mode))
 
 (use-package all-the-icons
 	:straight t
-  :if (display-graphic-p))
+	:if (display-graphic-p))
 
 ;; Generale sane defaults
-(setq inhibit-startup-buffer-menu t) ;; Don't show *Buffer list* when opening multiple files at the same time. 
+(setq inhibit-startup-buffer-menu t) ;; Don't show *Buffer list* when opening multiple files at the same time.
 (setq select-enable-clipboard t) ;; System clipboard
 (fset 'yes-or-no-p 'y-or-n-p) ;; No more typing the whole yes or no. Just y or n will do.
 
@@ -54,8 +59,8 @@
 ;; default to utf-8 for all the things
 (set-charset-priority 'unicode)
 (setq locale-coding-system 'utf-8
-      coding-system-for-read 'utf-8
-      coding-system-for-write 'utf-8)
+			coding-system-for-read 'utf-8
+			coding-system-for-write 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
@@ -69,12 +74,16 @@
 ;; no-littering doesn't set this by default so we must place
 ;; auto save files in the same path as it uses for sessions
 (setq auto-save-file-name-transforms
-      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+			`((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
 (setq create-lockfiles nil)
+
 (use-package doom-themes
 	:straight t
 	:config
-	  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-nord-aurora t)
-	)
+		(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+				doom-themes-enable-italic t) ; if nil, italics is universally disabled
+	(load-theme 'doom-nord-aurora t))
+
+;;(use-package solo-jazz-theme
+;;	:config
+;;	(load-theme 'solo-jazz t))
