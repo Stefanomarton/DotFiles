b27a7b8482835ed8c9d46fe1eedbc8bb6085f88c
Stefanomarton <sstefanomarton@gmail.com>
2023-02-07 20:02:36 +0100

    fix: rules for megasync client

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -346,7 +346,7 @@ ruled.client.connect_signal("request::rules", function()
 	ruled.client.append_rule({
 		id = "floating",
 		rule_any = {
-			instance = { "copyq", "pinentry", "Places" },
+			instance = { "copyq", "pinentry", "Places", "megasync" },
 			class = {
 				"Arandr",
 				"Blueman-manager",
@@ -357,6 +357,7 @@ ruled.client.connect_signal("request::rules", function()
 				"Wpa_gui",
 				"veromix",
 				"xtightvncviewer",
+				"MEGAsync",
 			},
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
 			-- and the name shown there might not match defined rules here.
@@ -364,8 +365,8 @@ ruled.client.connect_signal("request::rules", function()
 				"Event Tester", -- xev.
 			},
 			role = {
-				"AlarmWindow", -- Thunderbird's calendar.
-				"ConfigManager", -- Thunderbird's about:config.
+				-- "AlarmWindow", -- Thunderbird's calendar.
+				-- "ConfigManager", -- Thunderbird's about:config.
 				"pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
 			},
 		},
