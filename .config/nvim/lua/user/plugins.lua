a0a80de87dc4045842236d5e366c970c68a8b429
Stefanomarton <sstefanomarton@gmail.com>
2023-02-10 23:11:46 +0100

    feat: add leap bidirectional search

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -214,6 +214,10 @@ The Core plugins
 		require = "tpope/vim-repeat",
 		config = function()
 			require("leap").add_default_mappings()
+			vim.keymap.set("n", "s", function()
+				local current_window = vim.fn.win_getid()
+				require("leap").leap({ target_windows = { current_window } })
+			end)
 		end,
 	})
 
