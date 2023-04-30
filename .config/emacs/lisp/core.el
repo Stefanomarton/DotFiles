a39d93724e5fe1eed0802709a7cd30fa57d918e7
Stefanomarton <sstefanomarton@gmail.com>
2023-04-30 17:59:23 +0200

    chore: better evil and lsp settings

--- .config/emacs/lisp/core.el
+++ .config/emacs/lisp/core.el
@@ -6,7 +6,9 @@
 	:init
 	(setq completion-in-region-function 'consult-completion-in-region)
 	(setq evil-want-integration t)
+	(setq evil-echo-state nil)
 	(setq evil-want-keybinding nil)
+	(setq evil-want-empty-ex-last-command nil)
 	(setq evil-undo-system 'undo-fu)
 	(setq evil-search-module 'evil-search)
 	:config
@@ -207,7 +209,9 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
- (use-package company-auctex)
+(use-package tex
+	:straight auctex)
+
 (use-package reftex
 	:init
 	(add-hook 'LaTeX-mode-hook 'reftex-mode)
@@ -221,14 +225,15 @@
 (use-package corfu
 	;; Optional customizations
 	:custom
+	(corfu-history-mode)
 	(corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
 	(corfu-auto t)                 ;; Enable auto completion
 	(corfu-separator ?\s)          ;; Orderless field separator
-	;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+	(corfu-quit-at-boundary 'separator)   ;; Never quit at completion boundary
 	(corfu-quit-no-match 'separator)      ;; Never quit, even if there is no match
 	;; (corfu-preview-current nil)    ;; Disable current candidate preview
-	(corfu-preselect 'prompt)      ;; Preselect the prompt
-	;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+	(corfu-preselect 'valid)      ;; Preselect the prompt
+	;; (corfu-oN-exact-match nil)     ;; Configure handling of exact matches
 	;; (corfu-scroll-margin 5)        ;; Use scroll margin
 	(defun orderless-fast-dispatch (word index total)
 		(and (= index 0) (= total 1) (length< word 4)
@@ -251,6 +256,21 @@
 	:init
 	(global-corfu-mode))
 
+(use-package cape
+	:init
+	(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+	(add-to-list 'completion-at-point-functions #'cape-file)
+	(add-to-list 'completion-at-point-functions #'cape-elisp-block)
+	;;(add-to-list 'completion-at-point-functions #'cape-history)
+	;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+	;;(add-to-list 'completion-at-point-functions #'cape-tex)
+	;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+	;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+	;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+	;;(add-to-list 'completion-at-point-functions #'cape-dict)
+	;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+	;;(add-to-list 'completion-at-point-functions #'cape-line)
+	)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package solaire-mode
@@ -573,7 +593,47 @@
 	;; This directory will be used for `obsidian-capture' if set.
 	(obsidian-inbox-directory "Inbox"))
 
-(use-package all-the-icons-completion
+(use-package kind-icon
+	:ensure t
+	:after corfu
+	:custom
+	(kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
 	:config
-	(all-the-icons-completion-mode))
-(add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
+	(add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+;; Configure Tempel
+(use-package tempel
+	;; Require trigger prefix before template name when completing.
+	;; :custom
+	;; (tempel-trigger-prefix "<")
+
+	:bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+				 ("M-*" . tempel-insert))
+
+	:init
+
+	;; Setup completion at point
+	(defun tempel-setup-capf ()
+		;; Add the Tempel Capf to `completion-at-point-functions'.
+		;; `tempel-expand' only triggers on exact matches. Alternatively use
+		;; `tempel-complete' if you want to see all matches, but then you
+		;; should also configure `tempel-trigger-prefix', such that Tempel
+		;; does not trigger too often when you don't expect it. NOTE: We add
+		;; `tempel-expand' *before* the main programming mode Capf, such
+		;; that it will be tried first.
+		(setq-local completion-at-point-functions
+								(cons #'tempel-expand
+											completion-at-point-functions)))
+
+	(add-hook 'prog-mode-hook 'tempel-setup-capf)
+	(add-hook 'text-mode-hook 'tempel-setup-capf)
+
+	;; Optionally make the Tempel templates available to Abbrev,
+	;; either locally or globally. `expand-abbrev' is bound to C-x '.
+	;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+	;; (global-tempel-abbrev-mode)
+	)
+
+;; Optional: Add tempel-collection.
+;; The package is young and doesn't have comprehensive coverage.
+(use-package tempel-collection)
