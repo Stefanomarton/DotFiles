2dda6dc00e9d34654828b6ea7090848bd06080f9
Stefanomarton <sstefanomarton@gmail.com>
2023-10-31 20:26:04 +0100

    chore: update ibl v3

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -72,27 +72,25 @@ vim.g.lushwal_configuration =
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup({
-				char = "▏",
-				show_trailing_blankline_indent = false,
-				show_end_of_line = false,
-				show_first_indent_level = true,
-				use_treesitter = true,
-				show_current_context = true,
-				show_current_context_start = true,
-				buftype_exclude = { "terminal", "nofile" },
-				filetype_exclude = {
-					"help",
-					"packer",
-					"NvimTree",
-				},
-			})
-
-			vim.opt.list = true
-			vim.opt.listchars:append("eol:↴")
-			-- vim.opt.listchars:append("space:⋅")
-		end,
+		main= "ibl",
+		opts = {}
+		-- config = 
+		-- 		char = "▏",
+		-- 		show_trailing_blankline_indent = false,
+		-- 		show_end_of_line = false,
+		-- 		show_first_indent_level = true,
+		-- 		use_treesitter = true,
+		-- 		show_current_context = true,
+		-- 		show_current_context_start = true,
+		-- 		buftype_exclude = { "terminal", "nofile" },
+		-- 		filetype_exclude = {
+		-- 			"help",
+		-- 			"packer",
+		-- 			"NvimTree",
+		-- 		},
+			-- vim.opt.list = true,
+			-- vim.opt.listchars:append("eol:↴"),
+			-- -- vim.opt.listchars:append("space:⋅")
 	},
 	{
 		"declancm/cinnamon.nvim",
