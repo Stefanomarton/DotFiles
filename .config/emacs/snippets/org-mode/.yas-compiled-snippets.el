34bf92f98fa9ac08a8ea1572c1a5966dae1233c7
Stefanomarton <sstefanomarton@gmail.com>
2023-11-06 18:33:21 +0100

    feat: add org mode snippets

---
+++ .config/emacs/snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("im" "#+ATTR_LATEX: :width 7cm\n`(org-download-clipboard)(evil-normal-state)(evil-previous-visual-line)(evil-previous-visual-line)(evil-previous-visual-line)(evil-delete-line)`\n" "Center Environment" nil nil nil "/home/stefanom/.config/emacs/snippets/org-mode/prova" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Nov  6 12:49:51 2023
