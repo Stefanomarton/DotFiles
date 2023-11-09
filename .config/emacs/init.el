b8f8c749d03f85745fa6ea2770d5e78b1155489b
Stefanomarton <sstefanomarton@gmail.com>
2023-11-09 21:48:55 +0100

    chore: update modules

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -87,6 +87,7 @@
 
 (load-module "core")
 (load-module "evil")
+(load-module "file-management")
 (load-module "appearance")
 (load-module "modeline")
 (load-module "base-packages")
@@ -96,21 +97,7 @@
 (load-module "programming")
 (load-module "lsp")
 (load-module "document-production")
+(load-module "orgconfig")
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+(put 'dired-find-alternate-file 'disabled nil)
