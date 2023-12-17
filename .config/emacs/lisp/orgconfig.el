72f3f39405dc1cba64df512b5e07a645d334f3b5
Stefanomarton <sstefanomarton@gmail.com>
2023-12-17 23:29:56 +0100

    feat: override slug function to mantain denote naming convention

--- .config/emacs/lisp/orgconfig.el
+++ .config/emacs/lisp/orgconfig.el
@@ -6,7 +6,6 @@
   :straight t
   :ensure nil
   :hook
-  (org-mode . cdlatex-mode)
   (org-mode . org-cdlatex-mode)
   :custom
   (org-use-speed-commands t)
@@ -252,9 +251,8 @@
 
 
 (use-package org-roam
-  :defer t
   :commands (org-roam-node-find org-roam-capture consult-notes)
-  ;; :after dashboard
+
   :bind (("<leader>ob" . org-roam-buffer-toggle)
          ("<leader>of" . consult-org-roam-file-find)
          ("<leader>og" . consult-notes-search-in-all-notes)
@@ -263,6 +261,7 @@
          ("<leader>ok" . org-roam-capture)
          ("<leader>oc" . my/org-roam-node-find-courses)
          )
+
   :config
   ;; configuration for link buffer
   (add-to-list 'display-buffer-alist
@@ -288,6 +287,7 @@
 
   (setq org-roam-node-display-template
         (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
   (org-roam-db-autosync-mode)
 
   ;; If using org-roam-protocol
@@ -319,19 +319,19 @@
         '(("u" "uni" plain
            "%?"
            :if-new
-           (file+head "uni/%<%Y%m%d>__${slug}--%(hiddyn/select-tag).org"
+           (file+head "uni/%<%Y%m%d>--${slug}__%(hiddyn/select-tag).org"
                       "#+title: ${title}\n#+filetags: %(hiddyn/filetags)\n#+CREATED: %U\n#+LAST_MODIFIED: %U")
            :immediate-finish t
            :unnarrowed t)
           ("c" "course" plain
            "%?"
-           :if-new (file+head "uni/courses/%<%Y%m%d>__${slug}--%(hiddyn/select-tag).org"
+           :if-new (file+head "uni/courses/%<%Y%m%d>--${slug}__%(hiddyn/select-tag).org"
                               "#+title: ${title}\n#+filetags: %(hiddyn/filetags)\n#+CREATED: %U\n#+LAST_MODIFIED: %U")
            :immediate-finish t
            :unnarrowed t)
           ("n" "inbox" plain "%?"
            :if-new
-           (file+head "inbox/%<%Y%m%d>__${slug}--%(hiddyn/select-tag).org" "#+title: ${title}\n"
+           (file+head "inbox/%<%Y%m%d>--${slug}__%(hiddyn/select-tag).org" "#+title: ${title}\n"
                       "#+title: ${title}\n#+filetags: %(hiddyn/filetags)\n#+CREATED: %U\n#+LAST_MODIFIED: %U")
            :immediate-finish t
            :unnarrowed t)
@@ -344,14 +344,57 @@
            "%?"
            :target
            (file+head
-            "%(expand-file-name (or citar-org-roam-subdir \"\") org-roam-directory)/%<%Y%m%d>__${citar-citekey}--%(hiddyn/select-tag).org"
+            "%(expand-file-name (or citar-org-roam-subdir \"\") org-roam-directory)/%<%Y%m%d>--${citar-citekey}__%(hiddyn/select-tag).org"
             "#+title: ${citar-citekey}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n\n")
            :unnarrowed t)
           ("w" "work" plain "%?"
            :if-new
-           (file+head "work/${title}.org" "#+TITLE: ${title}\n#+FILETAGS: %^g :article:\n")
+           (file+head "work/${slug}.org" "#+TITLE: ${title}\n#+FILETAGS: %^g :article:\n")
            :immediate-finish t
            :unnarrowed t)))
+
+  ;; convert title to slug
+  (cl-defmethod org-roam-node-slug ((node org-roam-node))
+    "Return the slug of NODE."
+    (let ((title (org-roam-node-title node))
+          (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                             768 ; U+0300 COMBINING GRAVE ACCENT
+                             769 ; U+0301 COMBINING ACUTE ACCENT
+                             770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                             771 ; U+0303 COMBINING TILDE
+                             772 ; U+0304 COMBINING MACRON
+                             774 ; U+0306 COMBINING BREVE
+                             775 ; U+0307 COMBINING DOT ABOVE
+                             776 ; U+0308 COMBINING DIAERESIS
+                             777 ; U+0309 COMBINING HOOK ABOVE
+                             778 ; U+030A COMBINING RING ABOVE
+                             780 ; U+030C COMBINING CARON
+                             795 ; U+031B COMBINING HORN
+                             803 ; U+0323 COMBINING DOT BELOW
+                             804 ; U+0324 COMBINING DIAERESIS BELOW
+                             805 ; U+0325 COMBINING RING BELOW
+                             807 ; U+0327 COMBINING CEDILLA
+                             813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                             814 ; U+032E COMBINING BREVE BELOW
+                             816 ; U+0330 COMBINING TILDE BELOW
+                             817 ; U+0331 COMBINING MACRON BELOW
+                             )))
+      (cl-flet* ((nonspacing-mark-p (char)
+                   (memq char slug-trim-chars))
+                 (strip-nonspacing-marks (s)
+                   (string-glyph-decompose
+                    (apply #'string (seq-remove #'nonspacing-mark-p
+                                                (string-glyph-decompose s)))))
+                 (cl-replace (title pair)
+                   (replace-regexp-in-string (car pair) (cdr pair) title)))
+        (let* ((pairs `(
+                        ("[^[:alnum:][:digit:][:blank:]]" . "") ;; convert anything not alphanumeric
+                        (" " . "-")                   ;; remove sequential underscores
+                        ("--*" . "-")                   ;; remove sequential underscores
+                        ("^-" . "")                     ;; remove starting underscore
+                        ("-$" . "")))                   ;; remove ending underscore
+               (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
+          (downcase slug)))))
 
   (defun my/org-roam-node-find-courses ()
     "Show list of `org-roam-node-find' only under dirA."
