f44c22b58c2628ceb1a41a3b8e52d186190aa243
Stefanomarton <sstefanomarton@gmail.com>
2024-01-04 20:22:53 +0100

    fix: remove compiled snippets from repo

--- .config/emacs/snippets/fundamental-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `fundamental-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'fundamental-mode
-                     '(("todo" "`(insert (concat comment-start))` TODO: " "TODO Comment" nil nil nil "/home/stefanom/.config/emacs/snippets/fundamental-mode/comment_todo" nil nil)
-                       ("fix" "`(insert (concat comment-start))` FIX:" "FIX Comment" nil nil nil "/home/stefanom/.config/emacs/snippets/fundamental-mode/comment_fix" nil nil)))
-
-
-;;; Do not edit! File generated at Thu Dec 28 16:46:19 2023
--- .config/emacs/snippets/text-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `text-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'text-mode
-                     '(("title" "${1:$(make-string (string-width yas-text) ?\\=)}\n${1:Title}\n${1:$(make-string (string-width yas-text) ?\\=)}\n\n$0\n" "Title" nil nil nil "/home/stefanom/.config/emacs/snippets/text-mode/title" nil nil)))
-
-
-;;; Do not edit! File generated at Thu Dec 28 16:46:19 2023
