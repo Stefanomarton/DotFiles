6c36f90fe4512856df6086f76d49ddef22804627
Stefanomarton <sstefanomarton@gmail.com>
2024-01-04 20:19:29 +0100

    chore: update screen configuration

--- .config/qtileDesktop/config.py
+++ .config/qtileDesktop/config.py
@@ -1,4 +1,4 @@
-from libqtile import bar, layout, widget, hook, extension
+from libqtile import bar, layout, widget, hook, extension, qtile
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen, ScratchPad, DropDown
 # from libqtile.layout.xmonad import MonadThreeCol
 # from libqtile.extension import WindowList
@@ -123,12 +123,10 @@ keys = [
 
     # App launching
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Key([mod], "s", lazy.spawn("tofi-drun | zsh", shell=True), desc="Tofi launcher"),
     Key([mod], "s", lazy.spawn("dmenu_run -l 4", shell=True), desc="Tofi launcher"),
-    # Key([mod], "n", lazy.spawn("emacsclient -c", shell=True), desc="Emacs"),
     Key([mod, "shift"], "s", lazy.spawn('flameshot gui', shell=True), desc="Screenshot"),
-    Key([mod], "f", lazy.spawn("firefox"), desc="Firefox Browser"),
-    Key([mod, "shift"], "Return", lazy.spawn("emacsclient -c", shell=True), desc="Firefox Browser"),
+    Key([mod], "f", lazy.spawn("floorp"), desc="Firefox Browser"),
+    Key([mod, "shift"], "Return", lazy.spawn("emacsclient -c", shell=True), desc="emacs"),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
@@ -141,7 +139,6 @@ keys = [
     Key([mod], "b", lazy.hide_show_bar(), desc="Hides the bar"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
 keys.extend(
@@ -159,16 +156,56 @@ keys.extend(
 groups = [
     # Screen affinity here is used to make
     # sure the groups startup on the right screens
-    Group("1", screen_affinity=1),
-    Group("w", screen_affinity=0, matches=[Match(wm_class=["looking-glass-client"])]),
-    Group("2", screen_affinity=0),
-    Group("3", screen_affinity=0),
-    Group("4", screen_affinity=0),
-    Group("5", screen_affinity=0),
-    Group("6", matches=[Match(wm_class=["discord", "spotify"])], screen_affinity=1),
-    Group("7", screen_affinity=1),
-    Group("9", screen_affinity=1),
-]
+    Group("1",
+          label="browser",
+          # exclusive=True,
+          screen_affinity=0,
+          persist=True,
+          matches=[Match(wm_class=["floorp"])],
+          spawn="floorp"),
+    Group("w",
+          label="windows",
+          screen_affinity=0,
+          persist=True,
+          matches=[Match(wm_class=["looking-glass-client"])]),
+    Group("2",
+          label="editor",
+          screen_affinity=0,
+          persist=True,
+          matches=[Match(wm_class=["emacs"])]),
+    Group("3",
+          screen_affinity=0,
+          persist=True,),
+    Group("4",
+          screen_affinity=0,
+          persist=True,),
+    Group("5",
+          screen_affinity=1,
+          persist=True,),
+    Group("6",
+          screen_affinity=1,
+          persist=True,),
+    Group("7",
+          screen_affinity=1,
+          persist=True,
+          matches=[Match(wm_class=["ticktick"])],
+          ),
+    Group("8",
+          label="music",
+          screen_affinity=2,
+          persist=True,
+          spawn="spotify-launcher",
+          matches=[Match(wm_class=["spotify"])],
+          layout="verticaltile",
+          layout_opts= {
+                        "border_width":0,
+                        "border_focus": "#000000"
+                        },
+          ),
+    Group("9",
+          label="todo",
+          screen_affinity=2,
+          persist=True)]
 
 def go_to_group(name: str):
     def _inner(qtile):
@@ -179,8 +216,13 @@ def go_to_group(name: str):
         if name in '1234w': # remember to update for correct groups switching
             qtile.focus_screen(0)
             qtile.groups_map[name].toscreen()
-        else:
+
+        elif name in '567': # remember to update for correct groups switching
             qtile.focus_screen(1)
+            qtile.groups_map[name].toscreen()
+
+        else:
+            qtile.focus_screen(2)
             qtile.groups_map[name].toscreen()
 
     return _inner
@@ -195,9 +237,15 @@ def go_to_group_and_move_window(name: str):
             qtile.current_window.togroup(name, switch_group=False)
             qtile.focus_screen(0)
             qtile.groups_map[name].toscreen()
-        else:
+
+        elif name in "567":
             qtile.current_window.togroup(name, switch_group=False)
             qtile.focus_screen(1)
+            qtile.groups_map[name].toscreen()
+
+        else:
+            qtile.current_window.togroup(name, switch_group=False)
+            qtile.focus_screen(2)
             qtile.groups_map[name].toscreen()
 
     return _inner
@@ -206,7 +254,7 @@ for i in groups:
     keys.extend([
         Key([mod],
             i.name, lazy.function(go_to_group(i.name)),
-                desc="focus grou {}".format(i.name)),
+                desc="focus group {}".format(i.name)),
         Key([mod, "control"],
             i.name, lazy.function(go_to_group_and_move_window(i.name)),
                 desc="move windows to group {}".format(i.name)),
@@ -216,33 +264,78 @@ for i in groups:
                 ])
 
 
-groups.append(ScratchPad('filemanager',[DropDown('filemanager','zsh -c "kitty -e ranger"', y=0.05, height=0.8, opacity=1),]))
+groups.append(
+    ScratchPad('filemanager',
+               [
+               DropDown(
+                   'ranger','zsh -c "kitty -e ranger"', y=0.05, height=0.8, opacity=1
+                       ),
+               DropDown(
+                   'nemo', 'zsh -c "nemo"', y=0.05, height=0.8, opacity=1
+                       ),
+               DropDown(
+                   'qute', 'qutebrowser', x=0.70, height=1, width=0.3, opacity=1, on_focus_lost_hide=False
+               )
+               ]
+              )
+            )
 
 keys.extend([
-    Key([mod],'a',lazy.group['filemanager'].dropdown_toggle('filemanager')),
+    Key([mod],'a',lazy.group['filemanager'].dropdown_toggle('ranger')),
+    Key([mod],'n',lazy.group['filemanager'].dropdown_toggle('nemo')),
+    Key([mod],'g',lazy.group['filemanager'].dropdown_toggle('qute')),
 ])
 
-
 layouts = [
-    # layout.Columns(margin=4, border_focus=ColorB, border_normal=ColorZ, border_focus_stack=ColorB, border_normal_stack=ColorZ, border_width=2, split=False),
     # layout.Stack(num_stacks=2, border_focus=ColorB, border_normal=ColorZ, border_width=2,),
     # focuslayout.DistractionFree(),
     # layout.Tile(),
     # layout.Bsp(),
-    layout.MonadThreeCol(margin=[15,10,15,10], border_focus=ColorI, border_width=4, border_normal=ColorBG, main_centered=True, single_margin=[20,300,20,300], new_client_position="bottom"),
+    layout.MonadThreeCol(
+        align=1,
+        # margin=[15,8,15,8],
+        margin= 0,
+        border_focus=ColorI,
+        border_width=4,
+        border_normal=ColorBG,
+        main_centered=True,
+        single_margin=[20,300,20,300],
+        new_client_position="bottom",
+        single_border_width=0,
+        ratio=0.6,
+        max_ratio=0.55,
+        min_ratio=0.35,
+    ),
     # layout.MonadTall(),
-    layout.MonadWide(border_focus=ColorB, border_width=3, border_normal=ColorFG),
+    # layout.MonadWide(border_focus=ColorB, border_width=3, border_normal=ColorFG),
+    layout.Columns(
+        # margin=[15,10,15,10],
+        border_focus=ColorI,
+        border_normal=ColorZ,
+        border_focus_stack=ColorI,
+        border_normal_stack=ColorBG,
+        margin_on_single=[20,300,20,300],
+        border_width=4,
+        num_columns=3,
+        split=False,
+        wrap_focus_columns=True,
+        wrap_focus_rows=True,
+        wrap_focus_stacks=True,
+        
+    ),
     layout.Max(),
     # layout.RatioTile(),
     # layout.TreeTab(),
-    # layout.VerticalTile(),
+    layout.VerticalTile(
+        border_width=0,
+    ),
     # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
-    font="JetBrainsMono Nerd Font",
-    fontsize=12,
-    padding=3,
+    font="JuliaMono",
+    fontsize=10,
+    padding=5,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -250,95 +343,92 @@ screen_wallpaper = "~/.local/share/Wallpapers/Misc/wp9302457-3440x1440-summer-wa
 
 screens = [
     Screen(
+        bottom=bar.Bar(
+        [
+            widget.Spacer(length=5),
+            widget.CurrentLayout(
+                foreground=ColorFG,
+            ),
+            widget.Spacer(length=5),
+            widget.GroupBox(
+                highlight_method='block',
+                center_aligned= True,
+                background=ColorBG,
+                foreground="#FFFFFF",
+                inactive=ColorG,
+                other_current_screen_border=ColorD,
+                this_current_screen_border=ColorA,
+                other_screen_border=ColorH,
+                urgent_border=ColorFG,
+                urgent_text=ColorBG,
+                hide_unused= True,
+
+            ),
+            widget.Spacer(length=bar.STRETCH),
+            # widget.Prompt(),
+            # widget.WindowName(),
+            widget.TaskList(
+                highlight_method="block",
+                foreground=ColorFG,
+                border=ColorA,
+                title_width_method="uniform",
+                margin=0,
+            ),
+            # widget.Chord(
+            #     chords_colors={
+            #         "launch": ("#ff0000", "#ffffff"),
+            #     },
+            #     name_transform=lambda name: name.upper(),
+            # ),
+            # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+            # widget.Systray(),
+            widget.Spacer(length=bar.STRETCH),
+            # widget.Visualizer(
+            #     bar_colour=ColorB,
+            #     width=200,
+            #     bars=16,
+            #     channels="stereo",
+            #     bar_height=30,
+            # ),
+            # widget.Spacer(length=10),
+            widget.Clock(
+                format="%a %d-%m-%Y  %I:%M:%S",
+                foreground=ColorFG,
+                foreground_alert=ColorB
+            ),
+            widget.Spacer(length=10),
+            widget.Volume(
+                fmt='Vol:{}',
+                foreground=ColorFG,
+                check_mute_string="[off]",
+                volume_app="pavu",
+            ),
+            widget.Spacer(length=10),
+            widget.Wlan(
+                format='{essid}:{percent:2.0%}',
+                foreground=ColorFG,
+            ),
+            widget.Spacer(length=5),
+            widget.ThermalSensor(
+                format='CPU:{temp:.0f}{unit}',
+                foreground=ColorFG,
+            ),
+            widget.Spacer(length=10),
+            widget.Systray(
+                width=200
+            ),
+            widget.Spacer(length=10),
+            # widget.QuickExit(),
+        ],
+        20,
+        # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+        # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        background=ColorBG,
+        lenght=bar.STRETCH,
+        ),
+
         wallpaper= screen_wallpaper,
         wallpaper_mode="fill",
-        top=bar.Bar(
-            [
-                widget.Spacer(length=5),
-                widget.CurrentLayout(
-                    foreground=ColorFG,
-                ),
-                widget.Spacer(length=5),
-                widget.GroupBox(
-                    highlight_method='block',
-                    center_aligned= True,
-                    background=ColorBG,
-                    foreground="#FFFFFF",
-                    inactive=ColorG,
-                    other_current_screen_border=ColorD,
-                    this_current_screen_border=ColorA,
-                    other_screen_border=ColorH,
-                    urgent_border=ColorFG,
-                    urgent_text=ColorBG,
-                    hide_unused= True,
-
-                ),
-                widget.Spacer(length=bar.STRETCH),
-                # widget.Prompt(),
-                # widget.WindowName(),
-                widget.TaskList(
-                    highlight_method="block",
-                    foreground=ColorFG,
-                    border=ColorA,
-                    title_width_method="uniform",
-                    margin=0,
-                ),
-                # widget.Chord(
-                #     chords_colors={
-                #         "launch": ("#ff0000", "#ffffff"),
-                #     },
-                #     name_transform=lambda name: name.upper(),
-                # ),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.Systray(),
-                widget.Spacer(length=bar.STRETCH),
-                # widget.Visualizer(
-                #     bar_colour=ColorB,
-                #     width=200,
-                #     bars=16,
-                #     channels="stereo",
-                #     bar_height=30,
-                # ),
-                # widget.Spacer(length=10),
-                widget.Clock(
-                    format="%a %d-%m-%Y  %I:%M:%S",
-                    foreground=ColorFG,
-                    foreground_alert=ColorB
-                ),
-                widget.Spacer(length=10),
-                widget.Volume(
-                    fmt='Vol:{}',
-                    foreground=ColorFG,
-                    check_mute_string="[off]",
-                    volume_app="pavu",
-                ),
-                widget.Spacer(length=10),
-                widget.Wlan(
-                    format='{essid}:{percent:2.0%}',
-                    foreground=ColorFG,
-                ),
-                widget.Spacer(length=5),
-                widget.ThermalSensor(
-                    format='CPU:{temp:.0f}{unit}',
-                    foreground=ColorFG,
-                ),
-                widget.Spacer(length=10),
-                widget.Systray(
-                    width=200
-                ),
-                widget.Spacer(length=10),
-                # widget.QuickExit(),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-            background=ColorBG,
-            lenght=bar.STRETCH,
-        ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
     ),
     Screen(
         wallpaper= screen_wallpaper,
@@ -349,8 +439,6 @@ screens = [
         wallpaper_mode="fill",
     ),
 ]
-
-
 
 # Drag floating layouts.
 mouse = [
@@ -382,18 +470,11 @@ floating_layout = layout.Floating(
     ]
 )
 auto_fullscreen = True
-focus_on_window_activation = "smart"
+focus_on_window_activation = "urgent"
 reconfigure_screens = True
 
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-# wl_input_rules = {
-#         "type:keyboard": InputConfig(dwt=True, kb_repeat_delay=150, kb_repeat_rate=50, kb_variant="altgr-intl", kb_layout="us"),
-#         "*": InputConfig(tap=True, natural_scroll=False),
-#     }
-
 
 wmname = "LG3D"
