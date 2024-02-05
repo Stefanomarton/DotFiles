0c5108786a5ab7a899bdd76b1c4f65b8162d378e
Stefanomarton <sstefanomarton@gmail.com>
2024-02-05 15:10:21 +0100

    chore: treesit and indent barsd

--- .config/emacs/lisp/programming.el
+++ .config/emacs/lisp/programming.el
@@ -9,13 +9,23 @@
 ;;   (set-face-foreground 'highlight-indent-guides-character-face "#e3e3d3")
 ;;   )
 
+(use-package treesit
+  :straight (:type built-in)
+  :config
+  (customize-set-variable 'treesit-font-lock-level 4)
+  (setq major-mode-remap-alist
+        '((css-mode  . css-ts-mode)
+          (rust-mode . rust-ts-mode)
+          (python-mode . python-ts-mode)))
+  )
+
+
 ;; Python mode setup
 (use-package python-mode
   :commands python-mode
   :straight (:type built-in)
-  :mode ".py"
   :interpreter "python"
-  :init
+  :config
   (evil-define-key 'normal python-mode-map (kbd "<tab>") 'evil-shift-right-line)
   (evil-define-key 'normal python-mode-map (kbd "<backtab>") 'evil-shift-left-line)
   (evil-define-key 'visual python-mode-map (kbd "<tab>") 'evil-shift-right)
@@ -62,25 +72,16 @@
 (use-package indent-bars
   :straight (indent-bars :type git :host github :repo "jdtsmith/indent-bars")
   :config
-  ;; (setq
-  ;;  indent-bars-color '(highlight :face-bg t)
-  ;;  indent-bars-pattern "."
-  ;;  indent-bars-width-frac 0.2
-  ;;  indent-bars-pad-frac 0.1
-  ;;  indent-bars-zigzag nil
-  ;;  indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1) ; blend=1: blend with BG only
-  ;;  ;; indent-bars-highlight-current-depth '(:blend 0.5) ; pump up the BG blend on current
-  ;;  indent-bars-display-on-blank-lines t)
   :custom
   (indent-bars-treesit-support t)
   (indent-bars-no-descend-string t)
   (indent-bars-treesit-ignore-blank-lines-types '("module"))
-  ;; (indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
-  ;;   			                      list list_comprehension
-  ;;   			                      dictionary dictionary_comprehension
-  ;;   			                      parenthesized_expression subscript)))
+  (indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
+				                      list list_comprehension
+				                      dictionary dictionary_comprehension
+				                      parenthesized_expression subscript)))
   :hook
-  (python-mode yaml-mode) . (indent-bars-mode)) ; or whichever modes you prefer
+  (python-ts-mode yaml-mode) . (indent-bars-mode)) ; or whichever modes you prefer
 
 (use-package yaml-mode
   :mode "\\.yml\\'"
