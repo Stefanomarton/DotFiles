6400a0113d3aa857ca94a49e60f3602a7fe17110
Stefanomarton <sstefanomarton@gmail.com>
2024-04-06 15:03:30 +0200

    chore: fix treesit

--- .config/emacs/lisp/programming.el
+++ .config/emacs/lisp/programming.el
@@ -9,16 +9,35 @@
 ;;   (set-face-foreground 'highlight-indent-guides-character-face "#e3e3d3")
 ;;   )
 
-;; (use-package treesit
-;;   :straight (:type built-in)
-;;   :config
-;;   (customize-set-variable 'treesit-font-lock-level 4)
-;;   (setq major-mode-remap-alist
-;;         '((css-mode  . css-ts-mode)
-;;           (rust-mode . rust-ts-mode)
-;;           (python-mode . python-ts-mode)))
-;;   )
+(use-package treesit
+  :straight (:type built-in)
+  :config
+  (customize-set-variable 'treesit-font-lock-level 4)
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
+  (setq major-mode-remap-alist
+        '((css-mode  . css-ts-mode)
+          (rust-mode . rust-ts-mode)
+          (bash-mode . bash-ts-mode)
+          (yaml-mode . yaml-ts-mode)
+          (python-mode . python-ts-mode)))
+  )
 
 ;; Python mode setup
 (use-package python-mode
@@ -87,6 +106,13 @@
 (use-package yaml-mode
   :mode "\\.yml\\'"
   )
+
+(use-package yuck-mode
+  :mode "\\.yuck\\'"
+  )
+
+(use-package rust-mode
+  :mode "\\.rs\\'")
 
 (provide 'programming)
 
