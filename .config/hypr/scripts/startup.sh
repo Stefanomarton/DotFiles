19618805280b8dfd2ad8e2bb2293086bdd5918ba
Stefanomarton <sstefanomarton@gmail.com>
2024-04-07 11:23:19 +0200

    feat: w11 startup script
    chore: refactor
    
    chore: add startup script

---
+++ .config/hypr/scripts/startup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+
+# Check VM
+
+## Define the name of the VM
+vm_name="win11"
+
+### Check if the VM is running
+if ! virsh -c qemu:///system dominfo "$vm_name" | grep -q "State:\s*running"; then
+
+    ### Start VM using virsh
+    virsh -c qemu:///system start "$vm_name"
+
+fi
+
+## Check looking glass
+if ! pgrep -f "looking-glass-client" > /dev/null; then
+
+    ### Command to start Looking Glass
+    hyprctl dispatch exec "[workspace name:w11 silent;noanim] looking-glass-client"
+
+fi
+
+
+
+# Check Emacs server
+
+## Emacs server is running helper
+function is_emacs_server_running() {
+    emacsclient -e "(message \"\")" >/dev/null 2>&1
+}
+
+if ! is_emacs_server_running; then
+    ## Start emacs server if it's not already running
+    emacs --daemon
+fi
+
+
+
+# Kmonad
+
+## Start kmonad if hostname is laptop
+if [ "$(hostnamectl hostname)" = "laptop" ]; then
+    sleep 1 && kmonad .config/kmonad/laptop.kbd
+fi
