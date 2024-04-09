4f1fee63fa3040fd12457d232a633c33f2ad41c7
Stefanomarton <sstefanomarton@gmail.com>
2024-04-09 15:44:10 +0200

    fix: python mode

--- .config/emacs/lisp/lsp.el
+++ .config/emacs/lisp/lsp.el
@@ -41,9 +41,13 @@
 (use-package eglot
   :commands (eglot eglot-ensure)
   :hook
-  (python-mode . eglot-ensure)
+  (python-ts-mode . eglot-ensure)
   (LaTeX-mode . eglot-ensure)
   :config
+  (evil-define-key 'normal python-ts-mode-map (kbd "<tab>") 'evil-shift-right-line)
+  (evil-define-key 'normal python-ts-mode-map (kbd "<backtab>") 'evil-shift-left-line)
+  (evil-define-key 'visual python-ts-mode-map (kbd "<tab>") 'evil-shift-right)
+  (evil-define-key 'visual python-ts-mode-map (kbd "<backtab>") 'evil-shift-left)
   (setq eglot-workspace-configuration
         '((pylsp
            (plugins
