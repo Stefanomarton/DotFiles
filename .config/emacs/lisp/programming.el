ac4ebb9be41dac23d0a48822f0311200d73c20a5
Stefanomarton <sstefanomarton@gmail.com>
2024-04-10 18:48:08 +0200

    fix: treesit-mode

--- .config/emacs/lisp/programming.el
+++ .config/emacs/lisp/programming.el
@@ -1,55 +1,49 @@
 ;;; programming.el --- Programming languages configuration  -*- lexical-binding: t; -*-
 
-;; (use-package highlight-indent-guides
-;;   :hook (prog-mode . highlight-indent-guides-mode)
-;;   :custom (highlight-indent-guides-method 'character)
-;;   :init
-;;   ;; (set-face-background 'highlight-indent-guides-odd-face "#e3e3d3")
-;;   ;; (set-face-background 'highlight-indent-guides-even-face "dimgray")
-;;   (set-face-foreground 'highlight-indent-guides-character-face "#e3e3d3")
-;;   )
-
 (use-package treesit
   :straight (:type built-in)
+  :preface
+  (defun mp-setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+               (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+               (cmake . ("https://github.com/uyha/tree-sitter-cmake"))
+               (css . ("https://github.com/tree-sitter/tree-sitter-css"))
+               (elisp . ("https://github.com/Wilfred/tree-sitter-elisp"))
+               (go . ("https://github.com/tree-sitter/tree-sitter-go"))
+               (html . ("https://github.com/tree-sitter/tree-sitter-html"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+               (make . ("https://github.com/alemuller/tree-sitter-make"))
+               (markdown . ("https://github.com/ikatyang/tree-sitter-markdown"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+               (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
+
+  ;;Tree-sitter enabled major modes are
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (css-mode . css-ts-mode)
+             (typescript-mode . typescript-ts-mode)
+             (js2-mode . js-ts-mode)
+             (bash-mode . bash-ts-mode)
+             (css-mode . css-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode . json-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
+
   :config
-  (customize-set-variable 'treesit-font-lock-level 4)
-  (setq treesit-language-source-alist
-        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-          (rust "https://github.com/tree-sitter/tree-sitter-rust")
-          (cmake "https://github.com/uyha/tree-sitter-cmake")
-          (css "https://github.com/tree-sitter/tree-sitter-css")
-          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-          (go "https://github.com/tree-sitter/tree-sitter-go")
-          (html "https://github.com/tree-sitter/tree-sitter-html")
-          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-          (json "https://github.com/tree-sitter/tree-sitter-json")
-          (make "https://github.com/alemuller/tree-sitter-make")
-          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-          (python "https://github.com/tree-sitter/tree-sitter-python")
-          (toml "https://github.com/tree-sitter/tree-sitter-toml")
-          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-  (setq major-mode-remap-alist
-        '((css-mode  . css-ts-mode)
-          (rust-mode . rust-ts-mode)
-          (bash-mode . bash-ts-mode)
-          (yaml-mode . yaml-ts-mode)
-          (python-mode . python-ts-mode)))
-  )
-
-;; Python mode setup
-;; (use-package python
-;;   :commands python-mode
-;;   :straight (:type built-in)
-;;   :interpreter "python"
-;;   :config
-;;   (evil-define-key 'normal python-ts-mode-map (kbd "<tab>") 'evil-shift-right-line)
-;;   (evil-define-key 'normal python-ts-mode-map (kbd "<backtab>") 'evil-shift-left-line)
-;;   (evil-define-key 'visual python-ts-mode-map (kbd "<tab>") 'evil-shift-right)
-;;   (evil-define-key 'visual python-ts-mode-map (kbd "<backtab>") 'evil-shift-left)
-;;   )
+  (mp-setup-install-grammars))
 
 ;; Lua setup
 
@@ -89,21 +83,21 @@
   (add-to-list 'completion-at-point-functions #'yasnippet-capf))
 
 
-;; (use-package indent-bars
-;;   :straight (indent-bars :type git :host github :repo "jdtsmith/indent-bars")
-;;   :config
-;;   :custom
-;;   (indent-bars-treesit-support nil)
-;;   (indent-bars-no-descend-string t)
-;;   (indent-bars-treesit-ignore-blank-lines-types '("module"))
-;;   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
-;; 	                                   if_statement with_statement while_statement)))
-;;   ;; (indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
-;;   ;;   			                      list list_comprehension
-;;   ;;   			                      dictionary dictionary_comprehension
-;;   ;;   			                      parenthesized_expression subscript)))
-;;   :hook
-;;   (python-ts-mode yaml-ts-mode) . (indent-bars-mode)) ; or whichever modes you prefer
+(use-package indent-bars
+  :straight (indent-bars :type git :host github :repo "jdtsmith/indent-bars")
+  :config
+  :custom
+  (indent-bars-treesit-support t)
+  (indent-bars-no-descend-string t)
+  (indent-bars-treesit-ignore-blank-lines-types '("module"))
+  (indent-bars-treesit-scope '((python function_definition class_definition for_statement
+	                                   if_statement with_statement while_statement)))
+  ;; (indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
+  ;;   			                      list list_comprehension
+  ;;   			                      dictionary dictionary_comprehension
+  ;;   			                      parenthesized_expression subscript)))
+  :hook
+  (python-ts-mode yaml-ts-mode) . (indent-bars-mode)) ; or whichever modes you prefer
 
 (use-package yaml-mode
   :mode "\\.yml\\'"
