fbe028cd69b5bc4b35d32b23ea5849fa9bf48037
Stefanomarton <sstefanomarton@gmail.com>
2024-04-11 18:05:16 +0200

    chore: refactor and cleaning

--- .config/emacs/lisp/appearance.el
+++ .config/emacs/lisp/appearance.el
@@ -1,5 +1,4 @@
 ;;; appearance.el -*- lexical-binding: t; -*-
-;;;
 ;;; Configuration for making Emacs look pretty.
 
 (use-package emacs
@@ -15,19 +14,19 @@
 
   ;; Display line number relative and absolute
   (setq display-line-numbers-grow-only t)
-  (setq display-line-numbers-width-start t)
+  (setq display-line-numbers-width-start 15)
   (setq display-line-numbers-type 'relative)
 
   (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-  ;; (add-hook 'find-file-hook 'display-line-numbers-mode)
+  (add-hook 'markdown-mode-hook 'display-line-numbers-mode)
+  (add-hook 'latex-mode-hook 'display-line-numbers-mode)
 
   ;; Highlight the current line
   (add-hook 'prog-mode-hook 'hl-line-mode)
   (setq hl-line-sticky-flag nil)          ; Avoid seeing the bar in all windows
 
   ;; Line spacing
-  (setq line-spacing 1)
-  )
+  (setq line-spacing 0.12))
 
 ;; I like icons
 (use-package nerd-icons)
@@ -44,30 +43,7 @@
   :custom
   ;; (window-divider-default-right-width 3)
   ;; (window-divider-default-bottom-width 3)
-  (window-divider-default-places nil)
-  )
-
-;; (use-package spacious-padding
-;;   :config
-;;   ;; These are the defaults, but I keep it here for visiibility.
-;;   (setq spacious-padding-subtle-mode-line nil)
-;;   (setq spacious-padding-widths
-;;         '( :internal-border-width 25
-;;            :header-line-width 10
-;;            :mode-line-width 5
-;;            :tab-width 5
-;;            :right-divider-width 20
-;;            :scroll-bar-width 0))
-;;   (spacious-padding-mode 1))
-
-;; Theming
-
-;; (use-package doom-themes
-;;   ;; :defer .5
-;;   ;; :config
-;;   ;; (add-to-list 'custom-theme-load-path "~/.config/emacs/themes")
-;;   :config
-;;   (load-theme 'doom-nord t))
+  (window-divider-default-places nil))
 
 (use-package ewal
   :init
@@ -79,48 +55,12 @@
   (setq ewal-shade-percent-difference 10)
   (setq custom-safe-themes t)
   (add-to-list 'custom-theme-load-path "~/.config/emacs")
-  (load-theme 'pywal)
-  )
+  (load-theme 'pywal))
 
-;; Cool dashboard setting with minimal loading times
-;; (use-package dashboard
-;;   :if (< (length command-line-args) 2)
-;;   :custom
-;;   (initial-buffer-choice #'(lambda () (get-buffer-create "*dashboard*")))
-;;   (dashboard-banner-logo-title "Welcome Back Goblin")
-;;   (dashboard-startup-banner "~/.config/emacs/themes/logo.txt")
-;;   (dashboard-center-content t)
-;;   (dashboard-set-footer nil)
-;;   (dashboard-icon-type 'nerd-icons) ;; use `nerd-icons' package
-;;   ;; (setq dashboard-set-navigator t)
-;;   ;; (setq dashboard-set-heading-icons t)
-;;   (dashboard-set-file-icons t)
-;;   (dashboard-items nil)
-;;   ;; (dashboard-items '((recents  . 10)))
-;;   ;; (bookmarks . 5))
-;;   ;; (projects . 5)))
-;;   ;; (agenda . 5)
-;;   ;; (registers . 5)))
-;;   :config
-;;   ;; define custom mode map avoiding problems with lazyloading
-;;   (evil-define-key 'normal dashboard-mode-map
-;;     (kbd "<leader>ff") 'find-file
-;;     (kbd "<leader>fr") 'consult-recent-file
-;;     (kbd "<leader>b") 'consult-buffer
-;;     (kbd "f") 'find-file
-;;     (kbd "<leader>pp") 'projectile-switch-project
-;;     (kbd "c") (lambda ()
-;; 		        (interactive)
-;; 		        (let ((folder-path "~/.config/emacs"))
-;; 		          (find-file folder-path))))
-;;   (dashboard-setup-startup-hook)
-;;   )
 
 ;; Cool aspect
 (use-package mixed-pitch
-  :straight t
   :hook
-  (mixed-pitch-mode . my/mixed-pitch-spacing)
   (text-mode . mixed-pitch-mode)
   :config
   (add-to-list 'mixed-pitch-fixed-pitch-faces 'line-number)
@@ -130,12 +70,7 @@
   (add-to-list 'mixed-pitch-fixed-pitch-faces 'org-cite)
   (add-to-list 'mixed-pitch-fixed-pitch-faces 'error)
 
-  (setq mixed-pitch-set-height nil)
-
-  (defun my/mixed-pitch-spacing ()
-    (if mixed-pitch-mode
-        (setq line-spacing 0.12)
-      (setq line-spacing 0.0))))
+  (setq mixed-pitch-set-height nil))
 
 (use-package emacs
   :hook
@@ -160,7 +95,6 @@
 
 (use-package breadcrumb
   :hook
-  (prog-mode . breadcrumb-local-mode)
-  )
+  (prog-mode . breadcrumb-local-mode))
 
 (provide 'appearance)
