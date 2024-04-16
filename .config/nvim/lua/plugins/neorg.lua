4e2f1484869e4f682b6bdd715ebd938bed087b68
Stefanomarton <sstefanomarton@gmail.com>
2024-04-16 20:28:52 +0200

    chore: remove neorg

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,40 +1,40 @@
 return {
-	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		opts = {
-			load = {
-				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {}, -- Adds pretty icons to your documents
-				["core.dirman"] = { -- Manages Neorg workspaces
-					config = {
-						workspaces = {
-							notes = "~/notes",
-						},
-					},
-				},
-			},
-		},
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
-{
-  'nvim-orgmode/orgmode',
-  dependencies = {
-    { 'nvim-treesitter/nvim-treesitter', lazy = true },
-  },
-  event = 'VeryLazy',
-  config = function()
-    -- Load treesitter grammar for org
-    require('orgmode').setup_ts_grammar()
-
-    -- Setup treesitter
-    require('nvim-treesitter.configs').setup({
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'org' },
-      },
-      ensure_installed = { 'org' },
-    })
-  end,
-}
+-- 	{
+-- 		"nvim-neorg/neorg",
+-- 		build = ":Neorg sync-parsers",
+-- 		opts = {
+-- 			load = {
+-- 				["core.defaults"] = {}, -- Loads default behaviour
+-- 				["core.concealer"] = {}, -- Adds pretty icons to your documents
+-- 				["core.dirman"] = { -- Manages Neorg workspaces
+-- 					config = {
+-- 						workspaces = {
+-- 							notes = "~/notes",
+-- 						},
+-- 					},
+-- 				},
+-- 			},
+-- 		},
+-- 		dependencies = { { "nvim-lua/plenary.nvim" } },
+-- 	},
+-- {
+--   'nvim-orgmode/orgmode',
+--   dependencies = {
+--     { 'nvim-treesitter/nvim-treesitter', lazy = true },
+--   },
+--   event = 'VeryLazy',
+--   config = function()
+--     -- Load treesitter grammar for org
+--     require('orgmode').setup_ts_grammar()
+--
+--     -- Setup treesitter
+--     require('nvim-treesitter.configs').setup({
+--       highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = { 'org' },
+--       },
+--       ensure_installed = { 'org' },
+--     })
+--   end,
+-- }
 }
