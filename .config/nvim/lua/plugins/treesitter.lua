eab1c877a384b0c1f47ed3b84d5898d92f5b4b17
Stefanomarton <sstefanomarton@gmail.com>
2024-04-30 18:20:24 +0200

    chore: remove org parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -13,15 +13,6 @@ return {
 				},
 				endwise = { enable = true },
 			})
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'main',
-    files = { 'src/parser.c', 'src/scanner.c' },
-  },
-  filetype = 'org',
-}
 		end,
 		build = ":TSUpdate",
 	},
