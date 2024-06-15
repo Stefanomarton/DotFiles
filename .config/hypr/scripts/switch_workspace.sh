336b5f5d20387c2ea7afe9449d81c48d8ea4a6f3
Stefanomarton <sstefanomarton@gmail.com>
2024-06-15 18:16:28 +0200

    feat: eww widget on all screens

--- .config/hypr/scripts/switch_workspace.sh
+++ .config/hypr/scripts/switch_workspace.sh
@@ -1,5 +1,5 @@
 ewww close-popup ;
 hyprctl --batch dispatch workspace $1 &&
-eww open ws-popup &&
+eww open-many ws-popup-1 ws-popup-0 ws-popup-2 &&
 sleep $2 &&
-eww close ws-popup
+eww close ws-popup-1 ws-popup-0 ws-popup-2
