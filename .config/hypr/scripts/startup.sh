c99abcd91ee6bece7e2baa23f952146cca64f138
Stefanomarton <sstefanomarton@gmail.com>
2024-07-11 23:25:55 +0200

    chore: update startup

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -1,54 +1,51 @@
 #!/bin/bash
 
-
-
 # Check VM
 
 ## Define the name of the VM
-vm_name="win11"
+vm_name="work"
 
 ### Check if the VM is running
 if ! virsh -c qemu:///system dominfo "$vm_name" | grep -q "State:\s*running"; then
 
-    ### Start VM using virsh
-    virsh -c qemu:///system start "$vm_name"
+	### Start VM using virsh
+	virsh -c qemu:///system start "$vm_name"
+
+	killall looking-glass-client
+
+	sleep 10
+	hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
+
+else
+
+	### Command to start Looking Glass
+	killall looking-glass-client
+	hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
 
 fi
-
-### Command to start Looking Glass
-killall looking-glass-client
-hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
-
-
 
 # Check Emacs server
 
 ## Emacs server is running helper
 function is_emacs_server_running() {
-    emacsclient -e "(message \"\")" >/dev/null 2>&1
+	emacsclient -e "(message \"\")" >/dev/null 2>&1
 }
 
 if ! is_emacs_server_running; then
-    ## Start emacs server if it's not already running
-    emacs --daemon
+	## Start emacs server if it's not already running
+	emacs --daemon
 fi
-
-
 
 # Kmonad
 
 ## Start kmonad if hostname is laptop
 if [ "$(hostnamectl hostname)" = "laptop" ]; then
-    sleep 1 && kmonad .config/kmonad/laptop.kbd
+	sleep 1 && kmonad .config/kmonad/laptop.kbd
 fi
-
-
 
 # Eww
 eww daemon
 eww open bg-widget
 
-
-
 # Dropbox
-dropbox
+# dropbox
