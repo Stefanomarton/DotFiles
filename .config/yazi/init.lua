1ff8e1f638a09e5e06b9b1934763aa008452cf59
Stefanomarton <sstefanomarton@gmail.com>
2024-07-22 18:55:14 +0200

    chore: formattin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,28 +1,28 @@
 -- Bookmarks plugins
 require("yamb"):setup {
-  -- Optional, the path ending with path seperator represents folder.
-  bookmarks = bookmarks,
-  -- Optional, the cli of fzf.
-  cli = "fzf",
-  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
-  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  -- Optional, the path of bookmarks
-  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
-        (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+    -- Optional, the path ending with path seperator represents folder.
+    bookmarks = bookmarks,
+    -- Optional, the cli of fzf.
+    cli = "fzf",
+    -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+    keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    -- Optional, the path of bookmarks
+    path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/.config/yazi/bookmark")
 }
 
 -- show link path
 function Status:name()
-	local h = cx.active.current.hovered
-	if h == nil then
-		return ui.Span("")
-	end
+    local h = cx.active.current.hovered
+    if h == nil then
+        return ui.Span("")
+    end
 
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	return ui.Span(" " .. h.name .. linked)
+    local linked = ""
+    if h.link_to ~= nil then
+        linked = " -> " .. tostring(h.link_to)
+    end
+    return ui.Span(" " .. h.name .. linked)
 end
 
 -- remove position from status bar
@@ -35,16 +35,18 @@ end
 
 -- show size in status bar
 function Status:size()
-	local h = cx.active.current.hovered
-	if h == nil then
-		return ui.Line {}
-	end
+    local h = cx.active.current.hovered
+    if h == nil then
+        return ui.Line {}
+    end
 
-	local style = self.style()
-	return ui.Line {
-		ui.Span(" " .. ya.readable_size(h:size() or h.cha.length) .. " "):fg(style.bg):bg(THEME.status.separator_style.bg),
-		ui.Span(THEME.status.separator_close):fg(THEME.status.separator_style.fg),
-	}
+    local style = self.style()
+    return ui.Line {
+        ui.Span(" " .. ya.readable_size(h:size() or h.cha.length) .. " "):fg(style.bg):bg(
+            THEME.status.separator_style.bg
+        ),
+        ui.Span(THEME.status.separator_close):fg(THEME.status.separator_style.fg)
+    }
 end
 
 -- full-border plugin
@@ -52,11 +54,11 @@ require("full-border"):setup()
 
 -- zoxide plugin
 require("zoxide"):setup {
-    update_db = true,
-                        }
+    update_db = true
+}
 -- sync clipboard plugin
 require("session"):setup {
-	sync_yanked = true,
-                         }
+    sync_yanked = true
+}
 -- use arrow to move in parents folders
 require("parent-arrow")
