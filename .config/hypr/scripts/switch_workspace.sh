c1f8a89f1341a785c4b8a87ef920950cf953e471
Stefanomarton <sstefanomarton@gmail.com>
2024-08-30 19:38:39 +0200

    feat: use animatefocused

--- .config/hypr/scripts/switch_workspace.sh
+++ .config/hypr/scripts/switch_workspace.sh
@@ -1,5 +1,6 @@
 ewww close-popup
 hyprctl --batch dispatch workspace $1
+hyprctl --batch dispatch animatefocused
 eww open-many ws-popup-1 ws-popup-0 # ws-popup-2
 sleep $2
 eww close ws-popup-1 ws-popup-0 # ws-popup-2
