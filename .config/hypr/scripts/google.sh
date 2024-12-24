3d0a4aa9bfad6b0e1ded18d9323f845d5d632f9a
Stefanomarton <sstefanomarton@gmail.com>
2024-12-24 09:43:17 +0100

    fix: google it script

--- .config/hypr/scripts/google.sh
+++ .config/hypr/scripts/google.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Variables
 dmenu_command="wofi --show dmenu --conf ~/.config/wofi/minimal --style ~/.config/wofi/minimal.css"
+browser="firefox"
 
 # Prompt the user for input using dmenu
 search_query=$(echo "" | eval "$dmenu_command")
@@ -10,8 +12,8 @@ if [ -n "$search_query" ]; then
 	# Encode the query for use in a URL
 	encoded_query=$(echo "$search_query" | sed 's/ /%20/g')
 	# Open the search query in Firefox
-	firefox "https://www.google.com/search?q=$encoded_query"
+	$browser "https://www.google.com/search?q=$encoded_query"
 else
-	echo "No input provided. Exiting."
+	# No input provided. Exiting.
 	exit 1
 fi
