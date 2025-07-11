2e8b5db360e5c54ded777337afc671fe3cf92591
Stefanomarton <sstefanomarton@gmail.com>
2025-07-11 17:45:44 +0200

    chore: update hyprland config

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -20,19 +20,19 @@ vm_name="work"
 ### Check if the VM is running
 if ! virsh dominfo "$vm_name" | grep -q "State:\s*running"; then
 
-	### Start VM using virsh
-	virsh start "$vm_name"
+    ### Start VM using virsh
+    virsh start "$vm_name"
 
-	killall looking-glass-client
+    killall looking-glass-client
 
-	sleep 10
-	hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
+    sleep 10
+    hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
 
 else
 
-	### Command to start Looking Glass
-	killall looking-glass-client
-	hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
+    ### Command to start Looking Glass
+    killall looking-glass-client
+    hyprctl dispatch exec "[workspace name:w11 silent; noanim] looking-glass-client"
 
 fi
 
@@ -40,19 +40,19 @@ fi
 
 ## Emacs server is running helper
 function is_emacs_server_running() {
-	emacsclient -e "(message \"\")" >/dev/null 2>&1
+    emacsclient -e "(message \"\")" >/dev/null 2>&1
 }
 
 if ! is_emacs_server_running; then
-	## Start emacs server if it's not already running
-	emacs --daemon
+    ## Start emacs server if it's not already running
+    emacs --daemon
 fi
 
 # Kmonad
 
 ## Start kmonad if hostname is laptop
 if [ "$(hostnamectl hostname)" = "laptop" ]; then
-	sleep 1 && kmonad .config/kmonad/laptop.kbd
+    sleep 1 && kmonad .config/kmonad/laptop.kbd
 fi
 
 # Eww
@@ -65,4 +65,4 @@ eww daemon
 # 	disown
 # fi
 
-jamesdsp -t
+easyeffects
--- .config/hypr/scripts/switch_workspace.sh
+++ .config/hypr/scripts/switch_workspace.sh
@@ -2,6 +2,6 @@
 ewww close-popup
 hyprctl --batch dispatch workspace $1
 hyprctl --batch dispatch animatefocused
-eww open-many ws-popup-1 ws-popup-0 ws-popup-2
+eww open-many ws-popup-1 ws-popup-0
 sleep $2
-eww close ws-popup-1 ws-popup-0 ws-popup-2
+eww close ws-popup-1 ws-popup-0
